e1dae62cb921597f6cb347f63223a613c2389742
TheRiceCold <dalewaltergh@gmail.com>
2025-10-30 14:11:14 +0800

    components: Sidebar and Navigator

---
+++ src/scripts/allPosts.js
@@ -0,0 +1,26 @@
+export default [
+  { label: 'Notes', posts: [
+    'Notes 1',
+    'Notes 2',
+  ]},
+  { label: 'Learn with me', posts: [
+    'Subject 1',
+    'Subject 2',
+    'Subject 3',
+  ]},
+  { label: 'Insights', posts: [
+    'Insight 1',
+    'Insight 2',
+    'Insight 3',
+    'Insight 4',
+  ]},
+  { label: 'Research', posts: [
+    'Research 1',
+    'Research 2',
+    'Research 3'
+  ]},
+  { label: 'Writings', posts: [
+    'Poem',
+    'Letter'
+  ]}
+]
